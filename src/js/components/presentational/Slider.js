4edfea6bdb475452f26608ab2f2940cab4abaa0d
Danilo Vespa <d.vespa@gmail.com>
2018-01-08 19:13:11 -0200

    Modify HTML to show other data from the API

--- src/js/components/presentational/Slider.js
+++ src/js/components/presentational/Slider.js
@@ -5,12 +5,19 @@ import PropTypes from "prop-types";
 const PrintImage = ({imageOn, alt, title}) => {
   return (imageOn === "")? 'Loading...' : <img src={imageOn} alt={alt} title={title} />
 }
-
-const SlidePicture = ({ currentImage, alt, title}) => (
+let counter = 0;
+const SlidePicture = ({ currentImage, alt, title, description, authors}) => (
   <div className="slider__body">
-      <div className="slider__picture">
-          <PrintImage imageOn={currentImage}  alt={alt} title={title} />
-      </div>
+    <h1>{title}</h1>
+    <figure className="slider__picture">
+        <figcaption>{description}</figcaption>
+        <PrintImage imageOn={currentImage}  alt={alt} title={title} title={description}/>
+    </figure>
+    <ul className="slider_authors">
+       {authors.map(({name, description})=>
+            <li key={counter++}> {description}:{name}</li>
+        )}
+    </ul>
   </div>
 );
 
@@ -18,6 +25,7 @@ SlidePicture.propTypes = {
     alt: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     currentImage: PropTypes.string.isRequired,
+    authors: PropTypes.array.isRequired
 }
 
 export default SlidePicture;
